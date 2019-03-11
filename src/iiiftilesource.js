2b1201e4c24e49406a8ccb575ccccf9d9486a7f1
Elie Roux <elie.roux@telecom-bretagne.eu>
2019-03-11 19:02:17 +0100

    fix 1617

--- src/iiiftilesource.js
+++ src/iiiftilesource.js
@@ -362,9 +362,12 @@ $.extend( $.IIIFTileSource.prototype, $.TileSource.prototype, /** @lends OpenSea
         } else {
             iiifQuality = "default." + this.usedFormat;
         }
-
         if ( levelWidth < tileWidth && levelHeight < tileHeight ){
-            iiifSize = levelWidth + ",";
+            // max will be canonical in 3.0
+            iiifSize = "max";
+            iiifRegion = 'full';
+        } else if ( x == 0 && y == 0 && levelWidth == this.width && levelHeight == this.height ) {
+            iiifSize = "max";
             iiifRegion = 'full';
         } else {
             iiifTileX = x * iiifTileSizeWidth;
